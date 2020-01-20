a3e3a1e1d312e18b8535d609db1af18ea79a00b8
donbreo <email@brandondabreo.com>
2020-01-21 01:35:24 +0530

    progress with node

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-const http =require('http');
+// const http =require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const server = http.createServer(app);
-server.listen(80);
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req, res, next) => {
+	res.send('<form action="/product" method="POST"><input type="text" name= "title"><button type="submit">Add product</button></form>');
+});
+app.post('/product',(req, res, next) => {
+	console.log(req.body);
+	res.redirect('/');
+});
+app.use('/brandon',(req, res, next) => {
+	console.log('IN another middleware');
+	res.redirect('http://brandondabreo.com');
+});
+app.use('/',(req, res, next) => {
+	console.log('IN the main folder');
+	res.send("<h1>Hello from the other side</h1>");
+	next();
+});
+
+
+/* const server = http.createServer(app);
+server.listen(80); */
+app.listen(80);
