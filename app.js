4e2087db99264fa994260415c2a5950e5b02c8b7
donbreo <donbreo@gmail.com>
2020-06-13 13:45:33 +0530

    update

--- app.js
+++ app.js
@@ -10,18 +10,17 @@ app.engine('hbs', expressHbs({layoutsDir: path.join(__dirname,'views/layouts/'),
 app.set('view engine', 'hbs');
 app.set('views','views');
 
-const adminData = require('./routes/admin');
+const errorController = require('./controllers/error')
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404',{pageTitle: 'Page not Found'});
-});
+app.use(errorController.error404);
 
 app.listen(80);
 
