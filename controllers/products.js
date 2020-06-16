332ec9f063be985b7bd4362508980fc943d02c8e
branabreo <branabreo@gmail.com>
2020-06-16 14:18:51 +0530

    update

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,5 @@
 //const products = [];
-const Product = require ('../models/product');
+const Product = require('../models/product');
 
 
 exports.getAddProduct = (req, res, next) => {
@@ -15,19 +15,23 @@ exports.getAddProduct = (req, res, next) => {
 exports.postAddProducts = (req, res, next) => {
 //	products.push({title: req.body.title});
 	const product = new Product(req.body.title);
+	product.save();
 	res.redirect('/');
 }
 
 exports.getProducts= (req, res, next) => {
 	// const products = adminRoutes.products;
-	res.render('shop', {
-prods: products, 
-pageTitle: 'Shop', 
-path:'/', 
-hasProducts: products.length > 0,
-activeShop: true,
-productCSS: true
-});
-}
+	const products = Product.fetchAll((products) => {
+		res.render('shop', {
+			prods: products, 
+			pageTitle: 'Shop', 
+			path:'/', 
+			hasProducts: products.length > 0,
+			activeShop: true,
+			productCSS: true
+			});
+	});
+	
+};
 
-exports.products = products;
+//exports.products = products;
--- models/product.js
+++ models/product.js
@@ -1,5 +1,5 @@
-const products = [];
-
+const fs = require('fs');
+const path = require('path')
 module.exports = class Product {
 
 	constructor(t){
@@ -8,10 +8,35 @@ module.exports = class Product {
 	}
 
 	save(){
-		products.push(this);
+		const p = path.join(
+		path.dirname(process.mainModule.filename), 
+		'data',
+		'products.json'
+		);
+		fs.readFile(p, (err, fileContent) => {
+			let products = [];
+			if (!err)
+			{
+				products = JSON.parse(fileContent);
+			}
+			products.push(this);
+			fs.writeFile(p, JSON.stringify(products),(err) => {
+				console.log(err);
+			});
+		});
 	}
 
-	static fetchAll(){
-		return this.products;
+	static fetchAll(cb){
+		const p = path.join(
+			path.dirname(process.mainModule.filename), 
+			'data',
+			'products.json'
+			);
+		fs.readFile(p,(err, fileContent) =>{
+			if(err) {
+				cb([]);
+			}
+			 cb(JSON.parse(fileContent));
+		});
 	}
-}
+};
