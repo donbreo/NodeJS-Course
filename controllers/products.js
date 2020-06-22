2c35ee7e769249469bc38815f1923e1d3da1c6d7
branabreo <branabreo@gmail.com>
2020-06-22 22:37:00 +0530

    update

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 
 
 exports.getAddProduct = (req, res, next) => {
-	res.render('add-product',{
+	res.render('admin/add-product',{
 		pageTitle: 'Add Product',
 		path: '/admin/add-product',
 		formsCSS: true,
@@ -22,7 +22,7 @@ exports.postAddProducts = (req, res, next) => {
 exports.getProducts= (req, res, next) => {
 	// const products = adminRoutes.products;
 	const products = Product.fetchAll((products) => {
-		res.render('shop', {
+		res.render('shop/product-list', {
 			prods: products, 
 			pageTitle: 'Shop', 
 			path:'/', 
